f6a76ad4ab55b712baaf4b395e8cd62955e572a0
Nodirbek <muminovnodirjon3@gmail.com>
2025-12-18 03:19:03 +0500

    PostgreSQL, subjects, CBV added

--- upskill/urls.py
+++ upskill/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path 
-from .views import index, course, course_detail
+from .views import IndexView, course, course_detail
 
 app_name = 'upskill'
 
 urlpatterns = [
-    path('', index, name='index'),
+    # path('', index, name='index'),
+    path('', IndexView.as_view(), name='index'),
     path('course/', course, name='course'),
+    path('course/<slug:subject_slug>', course, name='course'),
     path('course/detail/<slug:slug>', course_detail, name='detail'),
 ]
--- upskill/views.py
+++ upskill/views.py
@@ -1,16 +1,26 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Course, Subject, Content
+from django.views.generic import ListView
+from django.http import HttpRequest
 
 
-def index(request):
-    return render(request, 'upskill/index.html')
+class IndexView(ListView):
+    template_name = 'upskill/index.html'
+    context_object_name = 'subjects'
+    
+    def get_queryset(self):
+        subjects = Subject.objects.all()
+        return subjects
 
 
-def course(request):
-    courses = Course.objects.all()
-
+def course(request, subject_slug):
+    if subject_slug:
+        courses = Course.objects.filter(slug=subject_slug)
+    else:
+        courses = Course.objects.all()
+    
     content = {
-        'courses': courses,
+        'courses': courses.order_by('id'),
     }
     return render(request, 'upskill/course.html', content)
 
